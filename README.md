ff8a3024e6e6f77dec46e266ca3693a711051073
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2016-04-29 09:25:41 +0530

    GETting a single entity

--- README.md
+++ README.md
@@ -5,10 +5,17 @@
 * Run the Application.java As Spring Boot App
 * Navigating to http://localhost:8080/ yields Whitelabel Error Page
 * To change the embedded server's port, add server.port=${port:9090} to \src\main\resources\application.properties file
-* Add Model class with Attributes, getters, setters & deafult no arg constructor
+* Add Model class with Attributes, getters, setters & default no arg constructor
 * Add Dao class to add, edit, del, get model objects
-* @RestController (value="/rest/entities") 
-* @RequestMapping (method = RequestMethod.GET , produces = MediaType.APPLICATION_JSON_VALUE)
+* @RestController 
+* @RequestMapping (value="/rest/entities" method = RequestMethod.GET , produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity&lt;Collection&lt;T&gt;&gt; methodForGETtingAllObjects() {
 	return new ResponseEntity&lt;Collection&lt;T&gt;&gt;(..., HttpStatus.OK) 	}
 * Navigate to http://localhost:9090/users
+* @RequestMapping(value="/rest/entities/{entityId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity&lt;T&gt; getUserById(@PathVariable("entityId") int id) {
+  //If found
+    return new ResponseEntity&lt;T&gt;(..., HttpStatus.OK);
+  //else
+	return new ResponseEntity&lt;T&gt;(HttpStatus.NOT_FOUND);
+* Navigate to http://localhost:9090/users/1 & http://localhost:9090/users/2
--- src/main/java/in/mhatre/sagar/spring/rest/controllers/UserController.java
+++ src/main/java/in/mhatre/sagar/spring/rest/controllers/UserController.java
@@ -5,20 +5,34 @@ import java.util.Collection;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.mhatre.sagar.spring.rest.dao.UserDao;
+import in.mhatre.sagar.spring.rest.exceptions.UserDoesNotExistException;
 import in.mhatre.sagar.spring.rest.models.User;
 
-@RestController(value = "/rest/users")
+@RestController
 public class UserController {
 
 	private UserDao userDao = UserDao.getInstance();
 
-	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "rest/users", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Collection<User>> getAllUsers() {
 		return new ResponseEntity<Collection<User>>(userDao.getAllUsers(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "rest/users/{userId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> getUserById(@PathVariable("userId") int id) {
+		User user;
+		try {
+			user = userDao.getUserById(id);
+			return new ResponseEntity<User>(user, HttpStatus.OK);
+		} catch (UserDoesNotExistException e) {
+			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+		} 
+		
 	}
 }
